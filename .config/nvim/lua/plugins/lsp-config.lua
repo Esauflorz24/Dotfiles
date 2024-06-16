b9a1b60ef0aef520863ca45e561d9269e956b66f
Esauflorz24 <102836833+Esauflorz24@users.noreply.github.com>
2024-06-16 16:45:36 -0600

    config on attach function

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,35 +1,43 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "pyright" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			local servers = { "clangd", "tsserver", "pyright", "lua_ls" }
-			for _, lsp in ipairs(servers) do
-				lspconfig[lsp].setup({
-					capabilities = capabilities,
-				})
-			end
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "clangd", "pyright" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+            local function on_attach(client, bufnr)
+                local function opts(desc)
+                    return { buffer = bufnr, desc = "LSP " .. desc }
+                end
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts "hover")
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
+                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+                client.server_capabilities.signatureHelpProvider = false
+            end
+            local servers = { "clangd", "tsserver", "pyright", "lua_ls" }
+            for _, lsp in ipairs(servers) do
+                lspconfig[lsp].setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            end
+        end,
+    },
 }
