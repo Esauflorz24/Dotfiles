a0d315059af110ddf8821259d5c88f46326c3ce6
Esauflorz24 <102836833+Esauflorz24@users.noreply.github.com>
2024-06-16 16:17:12 -0600

    alpha update

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,31 +1,52 @@
 return {
-  "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-    }
+		dashboard.section.header.val = {
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                     ]],
+			[[       ████ ██████           █████      ██                     ]],
+			[[      ███████████             █████                             ]],
+			[[      █████████ ███████████████████ ███   ███████████   ]],
+			[[     █████████  ███    █████████████ █████ ██████████████   ]],
+			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+			[[                                                                       ]],
+		}
 
-    alpha.setup(dashboard.opts)
-  end,
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "New file", "<cmd>ene <CR>"),
+			dashboard.button("SPC s f", "Find file"),
+			dashboard.button("SPC s o", "Recently opened files"),
+			dashboard.button("SPC s r", "Resume Telescope"),
+			dashboard.button("SPC s g", "Find word"),
+			dashboard.button("SPC s b", "Open buffers"),
+		}
+		_Gopts = {
+			position = "center",
+			hl = "Type",
+		}
+
+		local function footer()
+			return "The programmatic expression of my will. I live for this shit"
+		end
+
+		dashboard.section.footer.val = footer()
+
+		dashboard.opts.opts.noautocmd = true
+
+		alpha.setup(dashboard.opts)
+	end,
 }
