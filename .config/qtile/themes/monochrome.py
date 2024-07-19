fe32015cf32502a72a696c9c6758be2e69262ed8
Esauflorz24 <102836833+Esauflorz24@users.noreply.github.com>
2024-07-19 12:34:05 -0600

    added monochrome theme

---
+++ .config/qtile/themes/monochrome.py
@@ -0,0 +1,21 @@
+colors = {
+    "hack":{
+        "black" : ["#000000"],
+        "gray1":["#080808"],
+        "gray2": ["#191919"],
+        "gray3": ["#2a2a2a"],
+        "gray4": ["#444444"],
+        "gray5": ["#555555"],
+        "gray6": ["#7a7a7a"],
+        "gray7": ["#aaaaaa"],
+        "gray8": ["#cccccc"],
+        "gray9": ["#dddddd"],
+        "red" : ["#d70000"],
+        "warn" : ["#ffaa88"],
+        "special": ["#789978"],
+        "hint": ["#7788aa"],
+        "lack": ["#7088aa"],
+        "luster": ["#deeeed"],
+        "amber" : ["#FFD65C"],
+    }
+}
