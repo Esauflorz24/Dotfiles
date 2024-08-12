2f16510287884824ce3f13ee922042ee764123b3
E$Z <102836833+Esauflorz24@users.noreply.github.com>
2024-08-12 16:29:36 -0600

    lsp stuff

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -11,7 +11,7 @@ return {
 		lazy = false,
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "pyright" },
+				ensure_installed = { "lua_ls", "clangd", "pyright","bashls","tsserver" },
 			})
 		end,
 	},
