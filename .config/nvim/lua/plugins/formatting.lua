3aecbc4b5a3757e4a766246803fcbbb52d74fafb
E$Z <102836833+Esauflorz24@users.noreply.github.com>
2025-02-27 13:03:24 -0600

    stuff

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,14 +1,46 @@
 return {
-	"nvimtools/none-ls.nvim",
+	"stevearc/conform.nvim",
+	event = { "VeryLazy" },
 	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.clang_format,
-                null_ls.builtins.formatting.shfmt,
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier", "prettierd" },
+				typescript = { "prettier", "prettierd" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				html = { "prettier" },
+				json = { "prettier" },
+				sh = { "shfmt", "beautysh" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+			formatters = {
+				["clang-format"] = {
+					prepend_args = {
+						"-style={ \
+                        IndentWidth: 4, \
+                        TabWidth: 4, \
+                        UseTab: Never, \
+                        AccessModifierOffset: 0, \
+                        IndentAccessModifiers: true, \
+                        PackConstructorInitializers: Never}",
+					},
+				},
 			},
 		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<leader>gf", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format Manually" })
 	end,
 }
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,82 +1,81 @@
 return {
 
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        local lspconfig = require("lspconfig")
+	"neovim/nvim-lspconfig",
+	lazy = false,
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local lspconfig = require("lspconfig")
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(ev)
-                local opts = { buffer = ev.buf, silent = true }
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-                vim.keymap.set("n", "<leader>srn", vim.lsp.buf.rename, opts)
-                vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            end,
-        })
-        capabilities.textDocument.foldingRange = {
-            dynamicRegistration = false,
-            lineFoldingOnly = true,
-        }
-        local signs = {
-            Error = " ",
-            Warn = " ",
-            Hint = " ",
-            Info = " ",
-        }
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				local opts = { buffer = ev.buf, silent = true }
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "<leader>srn", vim.lsp.buf.rename, opts)
+				vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			end,
+		})
+		capabilities.textDocument.foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true,
+		}
+		local signs = {
+			Error = " ",
+			Warn = " ",
+			Hint = " ",
+			Info = " ",
+		}
 
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
 
-        lspconfig.clangd.setup({
-            capabilities = capabilities,
-            cmd = {
-                "clangd",
-                "--offset-encoding=utf-16",
-            },
-        })
+		lspconfig.clangd.setup({
+			capabilities = capabilities,
+			cmd = {
+				"clangd",
+				"--offset-encoding=utf-16",
+			},
+		})
 
-        lspconfig.pyright.setup({
-            capabilities = capabilities,
-        })
+		lspconfig.pyright.setup({
+			capabilities = capabilities,
+		})
 
-        lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                },
-            },
-        })
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		})
 
-        lspconfig.tsserver.setup({
-            capabilities = capabilities,
-            init_options = {
-                preferences = {
-                    disableSuggestions = true,
-                },
-            },
-            filetypes = {
-                "javascript",
-                "javascriptreact",
-                "javascript.jsx",
-                "typescript",
-                "typescriptreact",
-                "typescript.tsx",
-            },
-        })
+		lspconfig.tsserver.setup({
+			capabilities = capabilities,
+			init_options = {
+				preferences = {
+					disableSuggestions = true,
+				},
+			},
+			filetypes = {
+				"javascript",
+				"javascriptreact",
+				"javascript.jsx",
+				"typescript",
+				"typescriptreact",
+				"typescript.tsx",
+			},
+		})
 
-        lspconfig.bashls.setup({
-            capabilities = capabilities,
-            cmd = { "bash-language-server", "start" },
-        })
-    end,
+		lspconfig.bashls.setup({
+			capabilities = capabilities,
+		})
+	end,
 }
