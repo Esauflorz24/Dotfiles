e610282be125920a557e3308a76f00b17225f1e2
E$Z <102836833+Esauflorz24@users.noreply.github.com>
2025-03-07 13:11:53 -0600

    updating lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,9 +2,38 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local lazy_status = require("lazy.status")
 		require("lualine").setup({
 			options = {
 				theme = "lackluster",
+				component_separators = { left = "|", right = "|" },
+				section_separators = { left = " ", right = " " },
+				icons_enabled = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "encoding" },
+					{ "fileformat" },
+					{ "filetype" },
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
 			},
 		})
 	end,
