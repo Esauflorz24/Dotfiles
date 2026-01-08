5e1cdb21c73b51ec9009aea73135776a82a09f55
E <102836833+Esauflorz24@users.noreply.github.com>
2026-01-08 12:03:45 -0600

    update colors

--- .config/qtile/themes/monochrome.py
+++ .config/qtile/themes/monochrome.py
@@ -1,10 +1,10 @@
 colors = {
-    "hack":{
-        "black" : ["#101010"],
-        "white" : ["#f7f7f7"],
-        "blue" : ["#2a7bde"],
-        "green" : ["#33d17a"],
-        "gray1":["#080808"],
+    "hack": {
+        "black": ["#000000"],
+        "white": ["#f7f7f7"],
+        "blue": ["#2a7bde"],
+        "green": ["#33d17a"],
+        "gray1": ["#080808"],
         "gray2": ["#191919"],
         "gray3": ["#2a2a2a"],
         "gray4": ["#444444"],
@@ -13,13 +13,13 @@ colors = {
         "gray7": ["#aaaaaa"],
         "gray8": ["#cccccc"],
         "gray9": ["#dddddd"],
-        "red" : ["#d70000"],
-        "warn" : ["#ffaa88"],
+        "red": ["#d70000"],
+        "warn": ["#ffaa88"],
         "special": ["#789978"],
         "hint": ["#7788aa"],
         "lack": ["#7088aa"],
         "luster": ["#deeeed"],
-        "amber" : ["#FFD65C"],
-        
+        "amber": ["#FFD65C"],
     }
 }
+
